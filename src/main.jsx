1ca5fd60c202f5c8a0c046821bdcb44895cf5e9a
Vikas Prajapat <148682091+Vikas-51@users.noreply.github.com>
2025-06-29 14:53:34 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './context/ThemeContext';
 import './style/styles.css';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <ThemeProvider>
       <App />
     </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
